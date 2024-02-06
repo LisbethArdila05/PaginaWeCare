47c54435dc8e01c74628aa0ab742dd3c119935ad
LisbethArdila05 <ardilabernal5@gmail.com>
2024-02-06 10:07:53 -0500

    news

---
+++ redirecction.js
@@ -0,0 +1,23 @@
+//redirection
+function redirectAboutUs(){
+    window.location.href = 'about-us.html';
+}
+function redirectMoreInfo(){
+    window.location.href = '../service/hospital-service.html';
+}
+function redirectOurService(){
+    window.location.href = 'industrial-cleaning.html';
+}
+function redirectMasterService(){
+    window.location.href = '../service/office-service.html';
+}
+//redirection news
+function rediDeepcleaning (){
+    window.location.href = '../news/deepcleaning.html'
+}
+function redigreencleaning (){
+    window.location.href = '../news/greencleaning.html'
+}
+function rediDisinfectant (){
+    window.location.href = '../news/DISINFECTANT.html'
+}
